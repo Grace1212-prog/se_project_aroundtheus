c2af6ea1d8b2cdb9649e55f6eedeb502d5b5333a
Grace Banda <gracebanda96@gmail.com>
2023-08-05 14:30:44 +0100

    git commit -m "Around the U.S project"

--- scripts/index.js
+++ scripts/index.js
@@ -26,3 +26,15 @@ const initialCards = [
 ];
 
 console.log(initialCards);
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const modalClose = document.querySelector("#modal-close-button");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+modalClose.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
