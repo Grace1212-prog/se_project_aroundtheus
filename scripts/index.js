fd5badf02653c73335e1b738d60f0c22ede82a06
Grace Banda <gracebanda96@gmail.com>
2023-08-24 22:51:50 +0100

    git commit -am "feat delete button"

--- scripts/index.js
+++ scripts/index.js
@@ -34,6 +34,12 @@ const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
+const addCardModal = document.querySelector("#add-card-modal");
+const addNewCardButton = document.querySelector(".profile__add-button");
+const addCardModalCloseButton = addCardModal.querySelector(".modal__close");
+const addCardForm = addCardModal.querySelector(".modal__form");
+//addCardForm.addEventListener("submit", handleAddCardFormSubmit);
+const cardTitleInput = document.querySelector("#card-title-input");
 
 const profileEditForm = profileEditModal.querySelector(".modal__form");
 
@@ -43,6 +49,7 @@ const cardTemplate =
 
 function closePopup() {
   profileEditModal.classList.remove("modal_opened");
+  addCardModal.classList.remove("modal_opened");
 }
 
 profileEditButton.addEventListener("click", () => {
@@ -62,10 +69,52 @@ profileEditForm.addEventListener("submit", (e) => {
   closePopup();
 });
 
+function openModal(modal) {
+  modal.classList.add("modal_opened");
+}
+
+addCardModal.addEventListener("click", () => {
+  cardTitleInput.value = addCardForm.textContent;
+  cardInput.value = cardUrlInput.textContent;
+  addCardModal.classList.add("modal_opened");
+});
+
+addNewCardButton.addEventListener("click", () => openModal(addCardModal));
+addCardModalCloseButton.addEventListener("click", () =>
+  closePopup(addCardModal)
+);
+
+function handleAddCardFormSubmit(evt) {
+  evt.preventDefault();
+  const name = cardTitleInput.value;
+  const link = cardUrlInput.value;
+  const cardData = { name, link };
+  const cardElement = getCardElement(cardData);
+  cardListEL.prepend(cardElement);
+  //renderCard({ name, link }, cardsWrap);
+  closePopup(addCardModal);
+}
+
 function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImageEL = cardElement.querySelector(".card__image");
   const cardTitleEL = cardElement.querySelector(".card__title");
+  const likeButton = cardElement.querySelector(".card__like-button");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  
+  //add the event listener to the delete button
+
+  
+
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card__like-button_active");
+  });
+
+  deleteButton.addEventListener("click", () => {
+    cardElement.remove("card__delete-button");
+  });
+
   cardImageEL.src = cardData.link;
   cardImageEL.alt = cardData.name;
   cardTitleEL.textContent = cardData.name;
